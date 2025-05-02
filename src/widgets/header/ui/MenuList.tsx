5ca527fecb1b560d44644473e2a69d720914011f
junik <wkdwnsdlr37@gmail.com>
2025-05-02 14:49:00 +0900

    Refactor: 중첩 등록이벤트처리

--- src/widgets/header/ui/MenuList.tsx
+++ src/widgets/header/ui/MenuList.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 // layer
 import Home from "@/shared/asset/home.svg?react";
 import Random from "@/shared/asset/random.svg?react";
-import Group from "@/shared/asset/group.svg?react";
 import Theme from "@/shared/asset/theme.svg?react";
 import { path } from "@/shared/consts/paths";
 
@@ -31,16 +30,6 @@ const MenuList = () => {
         <Random className="tw:w-5 tw:h-5 tw:fill-service-gray tw:group-hover:fill-service-secondary" />
         <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
           랜덤채팅
-        </span>
-      </Link>
-
-      <Link
-        to={`/${path.group}`}
-        className="tw:flex tw:justify-center tw:gap-3 tw:p-3 tw:group tw:cursor-pointer"
-      >
-        <Group className="tw:w-5 tw:h-5 tw:fill-service-gray tw:group-hover:fill-service-secondary" />
-        <span className="tw:min-w-15 tw:text-center tw:group-hover:text-service-secondary tw:c-text-theme-base tw:text-base tw:font-medium">
-          그룹채팅
         </span>
       </Link>
 
